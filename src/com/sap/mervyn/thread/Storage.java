88f99999c5fd630f2b476e8b392ee06cac1f6376
I347764 <mervyn.chen@sap.com>
2019-11-16 18:33:38 +0800

    Desc: JIRA: ECT-

--- src/com/sap/mervyn/thread/Storage.java
+++ src/com/sap/mervyn/thread/Storage.java
@@ -1,19 +1,63 @@
 package com.sap.mervyn.thread;
 
+import com.sap.mervyn.thread.util.Debug;
 import com.sap.mervyn.thread.util.Tools;
 
-import java.io.Closeable;
-import java.io.IOException;
-import java.io.RandomAccessFile;
+import java.io.*;
+import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class Storage implements Closeable, AutoCloseable {
     private final RandomAccessFile storeFile;
     private final FileChannel storeChannel;
-    private final AtomicLong totalWrites = new AtomicLong(0);
+    protected final AtomicLong totalWrites = new AtomicLong(0);
 
-    
+    public Storage(long fileSize, String fileShortName) throws IOException {
+        String fullFileName = System.getProperty("java.io.tmpdir") + "/"
+                + fileShortName;
+        String localFileName;
+        localFileName = createStoreFile(fileSize, fullFileName);
+        storeFile = new RandomAccessFile(localFileName, "rw");
+        storeChannel = storeFile.getChannel();
+    }
+
+    /**
+     * 将data中指定的数据写入文件
+     *
+     * @param offset
+     *          写入数据在整个文件中的起始偏移位置
+     * @param byteBuf
+     *          byteBuf必须在该方法调用前执行byteBuf.flip()
+     * @throws IOException
+     * @return 写入文件的数据长度
+     */
+    public int store(long offset, ByteBuffer byteBuf)
+            throws IOException {
+        int length;
+        storeChannel.write(byteBuf, offset);
+        length = byteBuf.limit();
+        totalWrites.addAndGet(length);
+        return length;
+    }
+
+    public long getTotalWrites() {
+        return totalWrites.get();
+    }
+
+    private String createStoreFile(final long fileSize, String fullFileName)
+            throws IOException {
+        File file = new File(fullFileName);
+        Debug.info("create local file:%s", fullFileName);
+        RandomAccessFile raf;
+        raf = new RandomAccessFile(file, "rw");
+        try {
+            raf.setLength(fileSize);
+        } finally {
+            Tools.silentClose(raf);
+        }
+        return fullFileName;
+    }
 
     @Override
     public synchronized void close() throws IOException {
@@ -21,5 +65,4 @@ public class Storage implements Closeable, AutoCloseable {
             Tools.silentClose(storeChannel, storeFile);
         }
     }
-
 }
